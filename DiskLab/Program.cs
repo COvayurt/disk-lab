8475e974f2eba47c38c026053823463f529b2bad
COvayurt <canberk.ovayurt@gmail.com>
2024-05-14 12:04:25 +0300

    initial commit

---
+++ DiskLab/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using DiskGazer.AppInterface;
+using DiskGazer.Common;
+using DiskGazer.Models;
+using DiskGazer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiskGazer.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDiskSpeedGazer diskSpeedGazer = DiskSpeedGeezerFactory.getDiskSpeedGazerInstance();
+            DiskInfo diskInfo = diskSpeedGazer.FindTheFastestDisk();
+            Console.WriteLine("Fastest Disk is : " + diskInfo.Name);
+            Console.WriteLine("Press key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
