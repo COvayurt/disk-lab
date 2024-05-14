652229ed8f278d8a7699a2215ae8556899bb494a
COvayurt <canberk.ovayurt@gmail.com>
2024-05-14 14:21:03 +0300

    Imported DiskGazer as a dll and made it work but with racing

--- DiskLab/obj/Debug/net8.0/DiskLab.AssemblyInfo.cs
+++ DiskLab/obj/Debug/net8.0/DiskLab.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiskLab")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8475e974f2eba47c38c026053823463f529b2bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiskLab")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiskLab")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
